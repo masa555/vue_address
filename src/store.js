e4b487a618194bfdb3e1a3d16e5665b20ae70c3e
masa555 <tyutyumasato@gmail.com>
2019-05-21 14:16:08 +0900

    add_address

--- src/store.js
+++ src/store.js
@@ -5,17 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    drawer: false
-
+    drawer: false,
+    addresses: []
   },
   mutations: {
     toggleSideMenu(state) {
       state.drawer = !state.drawer
+    },
+    addAddress(state, address) {
+      state.addresses.push(address)
     }
   },
   actions: {
-    toggleSideMenu({ commit }) {
+    toggleSideMenu({
+      commit
+    }) {
       commit('toggleSideMenu')
-  }
+    },
+    addAddress({
+      commit
+    }, address) {
+      commit('addAddress', address)
+    }
   }
 })
--- src/views/Addresses.vue
+++ src/views/Addresses.vue
@@ -4,6 +4,14 @@
         <v-flex xs12>
             <h1>連絡先一覧</h1>
         </v-flex>
+
+    <v-flex xs12 mt-5 mr-5 text-xs-right>
+        <router-link :to="{ name: 'address_edit' }">
+            <v-btn color="info">
+            連絡先追加
+            </v-btn>
+        </router-link>
+    </v-flex>
         <v-flex xs12 mt-5 justify-center>
             <v-data-table :headers='headers' :items='addresses'>
                 <template v-slot:items="props">
@@ -18,30 +26,21 @@
 </v-container>
 </template>
 <script>
-export default{
-    data(){
-        return{
-            headers:[
-                {text:'名前',value:'name'},
-                {text:'電話番号',value:'tel'},
-                {text:'メールアドレス',value:'email'},
-                {text:'住所',value:'address'},
-            ],
-        addresses: [
-        {
-                name: '友人１',
-                tel: '090-0000-1111',
-                email: 'sample1@mail.com',
-                address: '東京都渋谷区'
-        },
-        {
-                name: '友人２',
-                tel: '090-2222-3333',
-                email: 'sample2@mail.com',
-                address: '東京都品川区'
-            }
-        ]
-        }
+export default {
+created () {
+    this.addresses = this.$store.state.addresses
+},
+data () {
+    return {
+    headers: [
+        { text: '名前', value: 'name' },
+        { text: '電話番号', value: 'tel' },
+        { text: 'メールアドレス', value: 'email' },
+        { text: '住所', value: 'address' }
+    ],
+    addresses: []
     }
 }
+}
 </script>
+}
