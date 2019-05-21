557f5afb03ddf787f067fd696653cabdea7bfdf6
masa555 <tyutyumasato@gmail.com>
2019-05-21 16:15:21 +0900

    edit_menu

--- src/store.js
+++ src/store.js
@@ -3,91 +3,82 @@ import Vuex from 'vuex'
 import firebase from 'firebase'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-      state: {
-        login_user: null,
-        drawer: false,
-        addresses: []
-      },
-      mutations: {
-        setLoginUser(state, user) {
-          state.login_user = user
-        },
-        deleteLoginUser(state) {
-          state.login_user = null
-        },
-        toggleSideMenu(state) {
-          state.drawer = !state.drawer
-        },
-        addAddress(state, address) {
-          state.addresses.push(address)
-        }
-      },
-      actions: {
-        setLoginUser({
-          commit
-        }, user) {
-          commit('setLoginUser', user)
-        },
-        fetchAddresses({
-          getters,
-          commit
-        }) {
-          firebase.firestore().collection(`users/${getters.uid}/addresses`).get().then(snapshot => {
-            snapshot.forEach(doc => commit('addAddress', doc.data()))
-          })
-        },
-        login() {
-          const google_auth_provider = new firebase.auth.GoogleAuthProvider()
-          firebase.auth().signInWithRedirect(google_auth_provider)
-        },
-        logout() {
-          firebase.auth().signOut()
-        },
-        deleteLoginUser({
-          commit
-        }) {
-          commit('deleteLoginUser')
-        },
-        toggleSideMenu({
-          commit
-        }) {
-          commit('toggleSideMenu')
-        },
-        addAddress({
-        getters,
-        commit
-        }, address) {
-        if (getters.uid) {
-          firebase.firestore().collection(`users/${getters.uid}/addresses`).add(address).then(doc => {
-          commit('addAddress', {
+  state: {
+    login_user: null,
+    drawer: false,
+    addresses: []
+  },
+  mutations: {
+    setLoginUser(state, user) {
+      state.login_user = user
+    },
+    deleteLoginUser(state) {
+      state.login_user = null
+    },
+    toggleSideMenu(state) {
+      state.drawer = !state.drawer
+    },
+    addAddress(state, {
+      id,
+      address
+    }) {
+      address.id = id
+      state.addresses.push(address)
+    }
+  },
+  actions: {
+    setLoginUser({
+      commit
+    }, user) {
+      commit('setLoginUser', user)
+    },
+    fetchAddresses({
+      getters,
+      commit
+    }) {
+      firebase.firestore().collection(`users/${getters.uid}/addresses`).get().then(snapshot => {
+        snapshot.forEach(doc => commit('addAddress', {
           id: doc.id,
-          address
+          address: doc.data()
+        }))
+      })
+    },
+    login() {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(google_auth_provider)
+    },
+    logout() {
+      firebase.auth().signOut()
+    },
+    deleteLoginUser({
+      commit
+    }) {
+      commit('deleteLoginUser')
+    },
+    toggleSideMenu({
+      commit
+    }) {
+      commit('toggleSideMenu')
+    },
+    addAddress({
+      getters,
+      commit
+    }, address) {
+      if (getters.uid) {
+        firebase.firestore().collection(`users/${getters.uid}/addresses`).add(address).then(doc => {
+          commit('addAddress', {
+            id: doc.id,
+            address
           })
-            })
-          }
-        },
-        updateAddress({
-          getters,
-          commit
-        }, {
-          id,
-          address
-        }) {
-          if (getters.uid) {
-            firebase.firestore().collection(`users/${getters.uid}/addresses`).doc(id).update(address).then(() => {
-              commit('updateAddress', {
-                id,
-                address
-              })
-            })
-          }
-        }
-        },
+        })
+      }
+    }
+  },
   getters: {
     userName: state => state.login_user ? state.login_user.displayName : '',
     photoURL: state => state.login_user ? state.login_user.photoURL : '',
-    uid:state=>state.login_user ? state.login_user.uid :null,
+    uid: state => state.login_user ? state.login_user.uid : null,
+   getAddressById: state => id => state.addresses.find(address => address.id === id)
   }
 })
--- src/views/AddressForm.vue
+++ src/views/AddressForm.vue
@@ -26,6 +26,15 @@
 <script>
 import { mapActions } from 'vuex'
 export default {
+  created(){
+    if(!this.$route.params.address_id) return
+    const address=this.$store.getters.getAddressById(this.$route.params.address_id)
+    if (address){
+      this.address=address
+    }else{
+      this.$router.path({name:'addresses'})
+    }
+  },
   data () {
     return {
       address: {}
